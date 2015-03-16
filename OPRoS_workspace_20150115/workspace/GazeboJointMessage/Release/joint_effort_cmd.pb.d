c880b364645dcd68bf126f6ee74f566300619297
opros-wiki <opros.contact@gmail.com>
2015-03-16 15:53:10 +0900

    remove workspace_20150115

--- OPRoS_workspace_20150115/workspace/GazeboJointMessage/Release/joint_effort_cmd.pb.d
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-joint_effort_cmd.pb.d ./joint_effort_cmd.pb.o: ../joint_effort_cmd.pb.cc ../joint_effort_cmd.pb.h
-
-../joint_effort_cmd.pb.h:
-
--- OPRoS_workspace_20150115/workspace/GazeboJointMessage/Release/joint_status.pb.d
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-joint_status.pb.d ./joint_status.pb.o: ../joint_status.pb.cc ../joint_status.pb.h
-
-../joint_status.pb.h:
-
